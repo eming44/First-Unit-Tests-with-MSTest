3c3156dfd9ae5f42a4557f312ffcba40c8ff6151
Eming44 <eming44@abv.bg>
2019-05-15 11:32:24 +0300

    First unit test lecture

---
+++ FirstTests.UnitTesting/MathUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTests.UnitTesting
+{
+    public class MathUtils
+    {
+        public int Sum(IEnumerable<int> numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentException();
+            }
+            var sum = numbers.Sum(x => x);
+            return sum;
+        }
+    }
+}
---
+++ Utilities.Tests.FirstTests/Utilities/MathUtilsClass/Sum_Should.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FirstTests.UnitTesting;
+
+namespace Utilities.Tests.FirstTests.Utilities.MathUtilsClass
+{
+    [TestClass]
+    public class Sum_Should
+    {
+        [TestMethod]
+        public void ThrowAnArgumentException_WhenTheListIsNull()
+        {
+            var mathUtil = new MathUtils();
+            Assert.ThrowsException<ArgumentException>(() => mathUtil.Sum(null));
+        }
+    }
+}
